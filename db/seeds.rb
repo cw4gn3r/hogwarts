54cbdfadad25b3d111249965aa8ac071293fc533
Christoph Wagner <chwagner@gmail.com>
2013-12-30 15:28:40 -0800

    Fixing model relationships

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+House.create([
+  { name: 'Gryffindor' },
+  { name: 'Ravenclaw'  },
+  { name: 'Hufflepuff' },
+  { name: 'Slytherin'  }
+])
+
